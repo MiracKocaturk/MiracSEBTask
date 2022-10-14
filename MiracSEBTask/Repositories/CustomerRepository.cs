b88e265b4e5e040495a1c8f6391d6d8240b1df01
Mirac <mmirackocaturk@gmail.com>
2022-10-14 10:31:45 +0200

    Repository for the customer constructed. Initially get customers and get one customer.

---
+++ MiracSEBTask/Repositories/CustomerRepository.cs
@@ -0,0 +1,26 @@
+﻿using MiracSEBTask.Models;
+using System.Linq;
+using System.Net.Mail;
+
+namespace MiracSEBTask.Repositories
+{
+    public class CustomerRepository
+    {
+        private readonly List<Customer> customers = new List<Customer>()
+        {
+            new Customer { Id = 1, SocialSecurityNumber=  "8501011234",EmailAddress=  "test1@test.com",PhoneNumber = "+46731234567" },
+            new Customer { Id = 2, SocialSecurityNumber = "8501011235",EmailAddress = "test2@test.com",PhoneNumber = "+46731234567" },
+            new Customer { Id = 3, SocialSecurityNumber = "8501011236",EmailAddress = "test3@test.com",PhoneNumber = "+46731234567" }
+        };
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return customers;
+        }
+
+        public Customer GetCustomer(int id)
+        {
+            return customers.SingleOrDefault(customer => customer.Id == id);
+        }
+    }
+}
