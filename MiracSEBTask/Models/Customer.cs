acd90af6ea5cb0cb3dc3e3d36c64f527e50e389a
Mirac <mmirackocaturk@gmail.com>
2022-10-14 10:20:58 +0200

    Cleaned Up the Code for initial start and created the customer data model

---
+++ MiracSEBTask/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace MiracSEBTask.Models
+{
+    public record Customer
+    {
+        public int Id { get; init; }
+        public string SocialSecurityNumber { get; init; }
+
+        public string? EmailAddress { get; init; }
+
+        public string? PhoneNumber { get; init; }
+
+    }
+}
